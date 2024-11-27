4eb11250d15bab097afcd7085f2d3c0806430dd4
amyrmahdy <amyrmahdy1@gmail.com>
2024-11-27 22:39:42 +0330

    fix: Now if SINCE value is equal to ZERO it will be read date from .env file. Now everytime when application be intruptted, it remembers the checkpoint and next time it start from the checkpoint. Also override=True forces dotenv to overwrite the shell environment variables.

--- main.py
+++ main.py
@@ -4,27 +4,47 @@ import os
 import time
 import ccxt
 import dotenv
+from datetime import datetime
+from zoneinfo import ZoneInfo
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 
+
 # Load environment variables
 dotenv_file = dotenv.find_dotenv()
-dotenv.load_dotenv(dotenv_file)
+dotenv.load_dotenv(dotenv_file, override=True)
 
 # Environment keys
+SINCE_YEAR_KEY = 'SINCE_YEAR'
+SINCE_MONTH_KEY = 'SINCE_MONTH'
+SINCE_DAY_KEY = 'SINCE_DAY'
+SINCE_HOUR_KEY = 'SINCE_HOUR'
+SINCE_MINUTES_KEY = 'SINCE_MINUTES'
+SINCE_SECONDS_KEY = 'SINCE_SECONDS'
 SINCE_KEY = 'SINCE'
 INTERVAL_KEY = 'INTERVAL'
 TIMEDELTA_KEY = 'TIMEDELTA'
+TIMEZONE_KEY = 'TIMEZONE'
 INFLUX_BUCKET_KEY = 'INFLUX_BUCKET'
 INFLUX_ORG_KEY = 'INFLUX_ORG'
 INFLUX_TOKEN_KEY = 'INFLUX_TOKEN'
 INFLUX_URL_KEY = 'INFLUX_URL'
 
-INFLUX_BUCKET_VALUE = os.getenv(INFLUX_BUCKET_KEY)
-INFLUX_ORG_VALUE = os.getenv(INFLUX_ORG_KEY)
-INFLUX_TOKEN_VALUE = os.getenv(INFLUX_TOKEN_KEY)
-INFLUX_URL_VALUE = os.getenv(INFLUX_URL_KEY)
+
+
+SINCE_YEAR = int(os.getenv(SINCE_YEAR_KEY))
+SINCE_MONTH = int(os.getenv(SINCE_MONTH_KEY))
+SINCE_DAY = int(os.getenv(SINCE_DAY_KEY))
+SINCE_HOUR = int(os.getenv(SINCE_HOUR_KEY))
+SINCE_MINUTES = int(os.getenv(SINCE_MINUTES_KEY))
+SINCE_SECONDS = int(os.getenv(SINCE_SECONDS_KEY))
+INTERVAL = int(os.getenv(INTERVAL_KEY))
+TIMEDELTA = int(os.getenv(TIMEDELTA_KEY))
+INFLUX_BUCKET = os.getenv(INFLUX_BUCKET_KEY)
+INFLUX_ORG = os.getenv(INFLUX_ORG_KEY)
+INFLUX_TOKEN = os.getenv(INFLUX_TOKEN_KEY)
+INFLUX_URL = os.getenv(INFLUX_URL_KEY)
 
 # Enviroment values
 # INTERVAL='60'
@@ -35,8 +55,8 @@ INFLUX_URL_VALUE = os.getenv(INFLUX_URL_KEY)
 # 1 minute is plus 60000 milisecond
 # 1 hour is plus 3.6e+6  milisecond'
 
-INTERVAL_VALUE = int(os.getenv(INTERVAL_KEY))
-TIMEDELTA_VALUE = int(os.getenv(TIMEDELTA_KEY))
+
+
 
 # Fetch OHLCV data from the exchange
 def fetch_ohlcv_data(since, symbol='TON/USDT', timeframe='1m', limit=10, exchange_id='kucoin'):
@@ -63,9 +83,9 @@ def update_env_file(key,value):
 def write_to_influxdb(data):
     """Write candlestick data into InfluxDB."""
     try:
-        with InfluxDBClient(url=INFLUX_URL_VALUE,
-                            token=INFLUX_TOKEN_VALUE,
-                            org=INFLUX_TOKEN_VALUE) as client:
+        with InfluxDBClient(url=INFLUX_URL,
+                            token=INFLUX_TOKEN,
+                            org=INFLUX_TOKEN) as client:
             write_api = client.write_api(write_options=SYNCHRONOUS)
             
             for row in data:
@@ -79,7 +99,7 @@ def write_to_influxdb(data):
                         .field("volume", float(volume) ) \
                         .time(timestamp_ns)
                         
-                write_api.write(bucket=INFLUX_BUCKET_VALUE, record=point, org=INFLUX_ORG_VALUE)
+                write_api.write(bucket=INFLUX_BUCKET, record=point, org=INFLUX_ORG)
                 print(f"Written to InfluxDB: {point}")
 
     except Exception as e:
@@ -88,20 +108,26 @@ def write_to_influxdb(data):
 # Example usage
 if __name__ == "__main__":
 
+    # Initiate Date if SINCE value is equal to ZERO
+    SINCE = int(os.getenv(SINCE_KEY))
+    if not SINCE:
+        since_timestamp = int(datetime(SINCE_YEAR,SINCE_MONTH,SINCE_DAY,SINCE_HOUR,SINCE_MINUTES,SINCE_SECONDS,
+                                    tzinfo=ZoneInfo('Europe/London')).timestamp() * 1000)
+        update_env_file(SINCE_KEY,since_timestamp)    
 
     while True:
-        SINCE_VALUE = int(os.getenv(SINCE_KEY))
-        print(f"SINCE VALUE: {SINCE_VALUE} START")
+        SINCE = int(os.getenv(SINCE_KEY))
+        print(f"SINCE VALUE: {SINCE} START")
         print(f"Fetching OHLCV data since: {SINCE_KEY}")
-        ohlcv = fetch_ohlcv_data(SINCE_VALUE)
+        ohlcv = fetch_ohlcv_data(SINCE)
         if ohlcv:
             print("Writing to InfluxDB...")
             write_to_influxdb(ohlcv)
             
             # Update SINCE_KEY to fetch new data next time
-            SINCE_VALUE = int(ohlcv[-1][0] + TIMEDELTA_VALUE)
-            update_env_file(SINCE_KEY,SINCE_VALUE)    
+            SINCE = int(ohlcv[-1][0] + TIMEDELTA)
+            update_env_file(SINCE_KEY,SINCE)    
         else:
             print("No data fetched or an error occurred.")
-        print(f"SINCE VALUE: {SINCE_VALUE} FINISH")
-        time.sleep(10) # replace with INTERVAL_VALUE
+        print(f"SINCE VALUE: {SINCE} FINISH")
+        time.sleep(10) # replace with INTERVAL
