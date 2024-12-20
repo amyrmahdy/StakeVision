c9534988cc97ac3a5c1847199769588484b63296
amyrmahdy <amyrmahdy1@gmail.com>
2024-12-20 19:31:35 +0330

    Update ddl.sql tonusdt

--- postgresql/tonusdt/ddl.sql
+++ postgresql/tonusdt/ddl.sql
@@ -1,23 +1,23 @@
- create database stakevision;
+-- create database stakevision;
 
- create schema tonusdt;
+-- create schema tonusdt;
 
 create table tonusdt.hourly (
-	epoch int primary key,
-	open  int not null,
-	high int not null,
-	low int not null,
-	close int not null,
-	volume int not null
+	epoch bigint primary key,
+	open  real not null,
+	high real not null,
+	low real not null,
+	close real not null,
+	volume real not null
 );
 
 
-
-insert into stakevision.tonusdt.hourly values (1,2,3,4,5,6);
-
+--insert into stakevision.tonusdt.hourly values (1,2,3,4,5,6);
 
 
 select "open", "close" from stakevision.tonusdt.hourly;
 
-delete from stakevision.tonusdt.hourly
-where epoch = 1 ;
+--delete from stakevision.tonusdt.hourly
+--where epoch = 1 ;
+
+insert into stakevision.tonusdt.hourly values (1733950800000, 6.3162, 6.3209, 6.2901, 6.3134, 48581.4471);
