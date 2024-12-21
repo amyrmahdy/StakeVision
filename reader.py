42dd81c6e2b24e0828cdd981a0ad89e454f68799
amyrmahdy <amyrmahdy1@gmail.com>
2024-12-21 10:55:52 +0330

    Add reader.py

---
+++ reader.py
@@ -0,0 +1,34 @@
+#!./venv/bin/python3.12
+import os
+import dotenv
+import psycopg2
+import pandas as pd
+from datetime import timedelta
+import matplotlib.pyplot as plt
+
+# Load environment variables
+dotenv_file = dotenv.find_dotenv()
+dotenv.load_dotenv(dotenv_file, override=True)
+
+def read_query(table_name):
+    print("Creating conncetion:")
+    conn = psycopg2.connect(database=os.getenv('POSTGRESQL_DATABASE'),
+                            host=os.getenv('POSTGRESQL_HOST'),
+                            port=os.getenv('POSTGRESQL_PORT'),
+                            user=os.getenv('POSTGRESQL_USER'),
+                            password=os.getenv('POSTGRESQL_PASSWORD')
+                            )
+    conn.autocommit = True
+    cursor = conn.cursor()
+    print("Execute select query:")
+    query = f'''
+    SELECT * FROM {table_name} ;
+    '''
+    cursor.execute(query)
+    retrieved_rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    print("Conncetion closed.")
+    return retrieved_rows
+
+
